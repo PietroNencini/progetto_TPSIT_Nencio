e5a5169b872bd651e859051acc8c1f520e0518d9
Pietro Nencini <nencinipietro2006@gmail.com>
2024-10-09 09:11:42 +0200

    Working server

--- src/main/java/it/nencini/Main.java
+++ src/main/java/it/nencini/Main.java
@@ -3,6 +3,9 @@ package it.nencini;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.DataOutputStream;
 
 public class Main {
 
@@ -13,5 +16,19 @@ public class Main {
         Socket socket = ss.accept();
 
         System.out.println("Qualcuno è entrato nel server");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+        String received_data = in.readLine();
+        System.out.println("Messaggio ricevuto: " + received_data);
+
+        String upperString = received_data.toUpperCase();
+        out.writeBytes(upperString + '\n');
+
+        System.out.println("** AGGIORNATO E INVIATO LA STRINGA **");
+
+        socket.close();
+        ss.close();
     }
 }
