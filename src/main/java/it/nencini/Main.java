84f006373fd9522f2179f54993de7298dba0dd36
Pietro Nencini <nencinipietro2006@gmail.com>
2024-10-09 08:43:08 +0200

    First server createdf

--- src/main/java/it/nencini/Main.java
+++ src/main/java/it/nencini/Main.java
@@ -1,7 +1,17 @@
 package it.nencini;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args) throws IOException{      //? Il mio primo server in Java 
+        System.out.println("Benvenuto nel server");
+
+        ServerSocket ss = new ServerSocket(5763);
+        Socket socket = ss.accept();
+
+        System.out.println("Qualcuno è entrato nel server");
     }
 }
